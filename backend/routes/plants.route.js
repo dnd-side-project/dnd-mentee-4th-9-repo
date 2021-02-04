94c5fa95a25be6633ac3f6c330a23fdce4f3af95
geneaky <kaktus418@gmail.com>
2021-02-04 22:51:53 +0900

    mock-data

--- backend/routes/plants.route.js
+++ backend/routes/plants.route.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const router = express.Router()
 
-router.get('/curating',(req,res)=>{
+router.post('/curating',(req,res)=>{
     switch(req.body.plant){
         case '몬스테라':
             return res.json({

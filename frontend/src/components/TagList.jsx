14bf607ab793e3ec264fee2155c66e4777cc7f25
choisohyun <sohyun4283@naver.com>
2021-02-07 15:20:26 +0900

    [feat] add reactive style in TagList

--- frontend/src/components/TagList.jsx
+++ frontend/src/components/TagList.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from "styled-components";
+import {getVW} from "../lib/calculate";
 
 
 /*
@@ -14,24 +15,40 @@ function TagList({tagData}) {
 }
 
 const Wrapper = styled.ul`
-  margin: 0 16px;
+  margin: 0 24px;
+  
+  @media ${({theme}) => theme.devices.md} {
+    margin: 0 max(16px, ${getVW(24)});
+  }
 `
 
 const Tag = styled.li`
-  border-radius: 50.24px;
   border: 1px solid rgba(0, 0, 0, 0.15);;
-  height: 33.09796905517578px;
   display: inline-block;
-  padding: 6px 14px;
-  margin: 5px 4px;
+  background: ${({theme}) => theme.colors.white};
+  border-radius: 74.2px;
+  height: 49.5px;
+  margin: 5px 4px 5px 0;
+  padding: 9.5px 20px;
+  
+  @media ${({theme}) => theme.devices.md} {
+    border-radius: max(50px, ${getVW(74.2)});
+    height: max(33px, ${getVW(49.5)});
+    padding: max(6px, ${getVW(9.5)}) max(14px, ${getVW(20)});
+  }
 `
 
 const Text = styled.span`
   font-style: normal;
   font-weight: normal;
-  font-size: 14px;
-  line-height: 20px;
+  font-size: 21px;
+  line-height: 30px;
   color: rgba(0, 0, 0, 0.6);
+  
+  @media ${({theme}) => theme.devices.md} {
+    font-size: max(14px, ${getVW(21)});
+    line-height: max(20px, ${getVW(30)});
+  }
 `
 
 export default TagList;

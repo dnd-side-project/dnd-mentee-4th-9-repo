b45d26ff5133efc8004bee534dcb58f406e50854
Kimsehwan96 <sktpghks138@gmail.com>
2021-02-07 12:17:44 +0900

    [feat] add controller with eager-loading for tag & add contoroller

--- backend/controllers/plant.controller.js
+++ backend/controllers/plant.controller.js
@@ -1,21 +1,48 @@
 const path = require('path');
 const Plant = require('../models/plant.model');
+const Tag = require('../models/tag.model');
 
-const getListPlants = async (req, res) => {
-    //추후 로직 작성
-    //최신순, 인기순 정렬을 구현 쿼리 스트링 이용? 혹은 path param?
-    try {
-        const result = await Plant.findAll({
-            order: ['createdAt', 'DESC']
-        });
-        return res.json(result);
-    } catch (err) {
-        return res.status(500).json(err);
+const getListPlants = async (queryString) => {
+    switch (queryString) {
+        case "recent":
+            return await Plant.findAll({
+                include: Tag,
+                order : [['createdAt', 'DESC']]
+            })
+            break;
+        case "like":
+            return await Plant.findAll({
+                include: Tag,
+                order: [['likes', 'DESC']]
+            })
+            break;
+        case "view":
+            return await Plant.findAll({
+                include: Tag,
+                order: [['views', 'DESC']]
+            })
+        default:
+            return await Plant.findAll({
+                include: Tag,
+                order : [['createdAt', 'DESC']]
+            })
+            break;
+        //default action is same as recently order
     }
 }
 
-const getDetailPlant = (req, res) => {
-    //특정 식물의 자세한 내용을 리턴하는 로직 작성 필요
+const getDetailPlant = async (plantId) => {
+    const response = await Plant.findByPk((plantId), {
+        include : Tag
+    });
+    const views = response["views"];
+    //조회수 업데이트
+    await Plant.update({
+        views: views + 1
+    }, {
+        where: { id : plantId }
+    });
+    return response;
 }
 
 

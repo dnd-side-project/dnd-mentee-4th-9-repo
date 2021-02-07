c07c3810a889f535fdc9fea33befc531b64b9ba4
choisohyun <sohyun4283@naver.com>
2021-02-07 12:26:13 +0900

    [feat] add RecommentPlant component frame

---
+++ frontend/src/components/RecommentPlant.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from "styled-components";
+
+function RecommendPlant() {
+  return (
+    <>
+      <Wrapper>
+
+      </Wrapper>
+    </>
+  );
+}
+
+const Wrapper = styled.div``;
+
+export default RecommendPlant;

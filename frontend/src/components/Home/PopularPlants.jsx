1e93c538e36190a7bfa6bddd7277d66b4f8182f6
1000peach <_970813@daum.net>
2021-02-12 13:31:23 +0900

    [feat] add recommentPlant in PopularPlants

--- frontend/src/components/Home/PopularPlants.jsx
+++ frontend/src/components/Home/PopularPlants.jsx
@@ -2,12 +2,21 @@ import React from 'react';
 import SubHead from '../../styles/SubHead';
 import Section from '../Section';
 import styled from 'styled-components';
+import RecommendPlant from '../RecommentPlant';
+
+const plantData = {
+  img: 'https://seeat-image-dev-image-bucket.s3.ap-northeast-2.amazonaws.com/%E1%84%80%E1%85%AA%E1%86%AB%E1%84%8B%E1%85%B3%E1%86%B7%E1%84%8C%E1%85%AE%E1%86%A8.png',
+  title: '몬스테라',
+  description: '바쁜 일상 속 조용한 힐링',
+  tagData: ['💧💧', '#보통크기'],
+};
 
 function PopularPlants() {
   return (
     <Section width="lg" margin={200}>
       <Wrapper>
         <SubHead>요즘 뜨는 친구들</SubHead>
+        <RecommendPlant plant={plantData} />
       </Wrapper>
     </Section>
   );

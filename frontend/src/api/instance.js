085f74638910a4c0d1bea9ca73a967c2e78da9d3
1000peach <_970813@daum.net>
2021-02-15 03:44:01 +0900

    [feat] add axios instance and config

---
+++ frontend/src/api/instance.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+import {BASE_URL} from '../const/config';
+
+const instance = axios.create({
+  baseURL: BASE_URL,
+});
+
+const get = (path) => {
+  return new Promise((resolve, reject) => {
+    instance
+      .get(path)
+      .then(({data}) => resolve(data))
+      .catch((e) => reject(e.message));
+  });
+};
+
+const post = (path, params) => {
+  return new Promise((resolve, reject) => {
+    instance
+      .post(path, params)
+      .then(({data}) => resolve(data))
+      .catch((e) => reject(e.message));
+  });
+};
+
+export {BASE_URL, get, post};

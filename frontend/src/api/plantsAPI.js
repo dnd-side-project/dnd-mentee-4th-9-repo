7721348f63d3954ff3b00c1545af724ea1ae7ea3
1000peach <_970813@daum.net>
2021-02-16 19:46:40 +0900

    [feat] add plantsAPI/getAllPlants

--- frontend/src/api/plantsAPI.js
+++ frontend/src/api/plantsAPI.js
@@ -3,3 +3,7 @@ import {get} from './instance';
 export const getPlantDetail = (plantId) => {
   return get(`/plants/${plantId}`);
 };
+
+export const getAllPlants = (order) => {
+  return get(`/plants?order=${order}`);
+};

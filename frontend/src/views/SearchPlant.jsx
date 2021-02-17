375f823d83c623fd3c1b22477388146aa71bead0
choisohyun <sohyun4283@naver.com>
2021-02-17 23:12:10 +0900

    [feat] add SearchPlant detail style

--- frontend/src/views/SearchPlant.jsx
+++ frontend/src/views/SearchPlant.jsx
@@ -35,13 +35,21 @@ const SearchPlant = () => {
 
   return (
     <Wrapper>
-      <Section>
+      <Section width="lg" bgColor="lightGreen">
         <SearchInput>
+          <img src={`${process.env.PUBLIC_URL}/images/search-black.svg`} alt="search-plant" />
           <input type="text" placeholder="어떤 식물 친구를 찾으시나요?" />
         </SearchInput>
       </Section>
 
-      <Section width="lg" bgColor="">
+      <Section width="lg">
+        <ChooseHeader>
+          <img src={`${process.env.PUBLIC_URL}/images/filter.svg`} alt="choose keyword" />
+          <h1>선호하는 키워드를 선택해보세요</h1>
+        </ChooseHeader>
+      </Section>
+
+      <Section width="lg" bgColor="lightGray" margin={40}>
         <KeywordGroup>
           {tagData.map(({type, tags}) => (
             <KeywordField key={type}>
@@ -60,25 +68,120 @@ const Wrapper = styled.div`
 `;
 
 const SearchInput = styled.div`
+  position: relative;
   margin-top: 56px;
   margin-bottom: 20px;
   height: 80px;
+  img {
+    position: absolute;
+    top: 28px;
+    left: 38px;
+  }
+  input {
+    border-radius: 20px;
+    border: none;
+    height: 80px;
+    width: 100%;
+    padding: 22px 38px 22px 76px;
+    color: ${({theme}) => theme.colors.gray};
+    font-family: 'Iropke Batang', Batang, Serif;
+    font-size: 24px;
+    line-height: 36px;
+    letter-spacing: -0.05em;
+    :focus {
+      outline: none;
+    }
+  }
+
+  @media ${({theme}) => theme.devices.md} {
+    height: 40px;
+
+    img {
+      transform: scale(0.54);
+      top: 9px;
+      left: 12px;
+    }
+    input {
+      border-radius: 5px;
+      height: 40px;
+      width: 100%;
+      padding: 11px 19px 11px 36px;
+      font-size: 14px;
+      line-height: 18.2px;
+    }
+  }
+`;
+
+const ChooseHeader = styled.header`
+  position: relative;
+  height: 80px;
+  display: flex;
+  align-items: center;
+
+  img {
+    position: absolute;
+    margin-right: 34px;
+  }
+  h1 {
+    font-family: 'Iropke Batang', Batang, Serif;
+    font-size: 24px;
+    line-height: 46px;
+    letter-spacing: -0.05em;
+    color: ${({theme}) => theme.colors.green};
+    padding-left: 34px;
+  }
+
+  @media ${({theme}) => theme.devices.md} {
+    height: 50px;
+
+    img {
+      transform: scale(0.54);
+      margin-right: 5px;
+    }
+    h1 {
+      font-size: 14px;
+      line-height: 21px;
+      padding-left: 18px;
+    }
+  }
 `;
 
 const KeywordGroup = styled.fieldset`
-  height: 324px;
+  height: 404px;
   display: flex;
   flex-direction: column;
   flex-wrap: wrap;
+
+  @media ${({theme}) => theme.devices.md} {
+    height: auto;
+  }
 `;
 
 const KeywordField = styled.div`
   display: flex;
   flex-direction: row;
-  align-items: center;
-  height: 68px;
+  align-items: baseline;
+  height: 88px;
 
   h2 {
+    width: 127px;
+    font-size: 28px;
+    line-height: 42px;
+    letter-spacing: -0.05em;
+    color: ${({theme}) => theme.colors.darkGray};
+  }
+
+  @media ${({theme}) => theme.devices.md} {
+    height: 31.5px;
+
+    h2 {
+      width: 65px;
+      font-size: 14px;
+      line-height: 21px;
+    }
+    & + & {
+      margin-top: 10px;
+    }
   }
 `;
 

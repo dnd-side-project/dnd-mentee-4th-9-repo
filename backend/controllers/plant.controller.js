8e93705ffdc611dcd7979680eeb35f3cd0299371
geneaky <kaktus418@gmail.com>
2021-02-18 16:54:03 +0900

    seperate controller to service layer

--- backend/controllers/plant.controller.js
+++ backend/controllers/plant.controller.js
@@ -2,220 +2,63 @@ const Plant = require('../models/plant.model');
 const Tag = require('../models/tag.model');
 const sequelize = require('sequelize');
 const Op = sequelize.Op;
+const {recentOrderPlants,viewOrderPlants,allOrderPlants,detailPlant,getCuratingResult,searchPlantName,searchPlantTag} = require('../services/plantService');
 
 const getListPlants = async (req, res, next) => {
-    if (req.query.order === "recent") {
-        try {
-            const result = await Plant.findAll({
-                include: [{
-                    model: Tag,
-                    attributes: {
-                        exclude: ['createdAt', 'updatedAt']
-                    },
-                    through: {
-                        attributes: []
-                    }
-                }],
-                order: [['createdAt', 'DESC']]
-            });
-            res.json(result);
-        } catch (error) {
-            next(error)
+    const plantDTO = req.query.order;
+    let result;
+    try{
+        switch(plantDTO){
+            case "recent":
+                result = await recentOrderPlants();
+                return res.status(200).json(result);
+            case "view":
+                result = await viewOrderPlants();
+                return res.status(200).json(result);
+            default:
+                result = await allOrderPlants();
+                return res.status(200).json(result);
         }
-    } else if (req.query.order === "view") {
-        try {
-            const result = await Plant.findAll({
-                include: [{
-                    model: Tag,
-                    attributes: {
-                        exclude: ['createdAt', 'updatedAt']
-                    },
-                    through: {
-                        attributes: []
-                    }
-                }],
-                order: [['views', 'DESC']]
-            })
-            res.json(result);
-        } catch (error) {
-            next(error);
-        }
-    } else {
-        try {
-            const result = await Plant.findAll({
-                include: [{
-                    model: Tag,
-                    attributes: {
-                        exclude: ['createdAt', 'updatedAt']
-                    },
-                    through: {
-                        attributes: []
-                    }
-                }],
-                order: [['createdAt', 'DESC']]
-            });
-            res.json(result);
-        } catch (error) {
-            next(error);
-        }
+    }catch(error){
+        next(error);
     }
 }
 
 const getDetailPlant = async (req, res, next) => {
+    const plantDTO = req.params.plantId;
     try {
-        const baseResult = await Plant.findByPk((req.params.plantId), {
-            attributes : {
-                exclude: ['thumbnailPath', 'createdAt', 'updatedAt']
-            },
-            include: [{
-                model: Tag,
-                attributes: {
-                    exclude: ['createdAt', 'updatedAt']
-                },
-                through: {
-                    where: { //PlantTags 에 있는 내용, 즉 태그의 특성이 없는 경우 (null) 제외하고 리턴
-                        imagePath: {
-                            [Op.ne]: null
-                        },
-                        description: {
-                            [Op.ne]: null
-                        }
-                    },
-                    attributes: ['type', 'imagePath', 'description'] //junction table(PlantTags 에서 받아올 내용)
-                }
-            }],
-        });
-
-        const additonalResult = await Plant.findByPk((req.params.plantId), { //모든 태그를 조회하기 위한 쿼리
-            attributes: [],
-            include: [{
-                model: Tag,
-                attributes: ['name'],
-                through: {
-                    attributes: []
-                }
-            }],
-        });
-
-        // Promise<Model>의 응답값을 JSON 오브젝트로 바꾸는 메서드 .get() 사용.
-        const tags = additonalResult.get({
-            plain: true
-        });
-
-        // ************추천 식물*********************
-
-        const tagArray = Object.values(tags)[0];
-        const recommendTag = [];
-        for (i in tagArray) {
-            recommendTag.push(tagArray[i].name);
-        }
-        // tag객체에서 시퀄라이스 함수에서 사용하기 편하도록 배열로 뽑아냅니다.
-        // 배열에서 특정 태그를 슬라이싱하는 것은 나중에 합니다.
-
-        const allPlants = await Plant.findAll({
-            attributes: ['id', 'name', 'thumbnailPath', 'description'],
-            where: {
-                id: {
-                    [Op.ne]: req.params.plantId
-                }
-            },
-            include: [{
-                model: Tag,
-                attributes: ['id', 'name', 'type'],
-                through: {
-                    attributes: []
-                }
-            }]
-        });
-
-        //몇개의 태그가 매칭되는지 계산하는 함수
-        const isRecommended = (Plant) => {
-            const tags = Plant.get({
-                plain: true
-            })['Tags']
-
-            const count = tags
-                .map(tag => tag.name)
-                .filter(item => recommendTag.includes(item))
-                .length
-            return count > 4
-        }
-
-        const recommendPlants = allPlants.filter(plant => isRecommended(plant));
-
-        // ****************************************
-
-        const detailResult = baseResult.get({
-            plain: true
-        });
-
-        detailResult.allTags = tags.Tags;
-        detailResult.recommendPlants = recommendPlants;
-
-        await Plant.update({
-            views: sequelize.literal('views + 1')
-        }, {
-            where: {id: req.params.plantId}
-        });
-        res.json(detailResult);
+        const detailResult = await detailPlant(plantDTO);
+        res.status(200).json(detailResult);
     } catch (error) {
         next(error);
     }
 }
 
 const curatingResult = async (req, res, next) => {
+    const plantDTO = req.query.result;
     try {
-        const resultPlant = await Plant.findOne({
-            attributes: ['id', 'name', 'description', 'ment', 'imagePath'],
-            where: {name: req.query.result},
-            include: [{
-                model: Tag,
-                attributes: ['id', 'name'],
-                through: {
-                    attributes: []
-                }
-            }]
-        });
-        res.json(resultPlant);
+        const resultPlant = await getCuratingResult(plantDTO);
+        res.status(200).json(resultPlant);
     } catch (error) {
         next(error);
     }
 }
 
 const searchByPlantName = async (req, res, next) => {
+    const plantDTO = req.body.keyword;
     try {
-        const searchResult = await Plant.findAll({
-            attributes: ['id', 'name', 'description', 'thumbnailPath'],
-            where: {name: {[Op.like]: `${req.body.keyword}%`}},
-            include: [{
-                model: Tag,
-                attributes: ['id', 'name'],
-                through: {
-                    attributes: []
-                }
-            }]
-        });
-        res.json(searchResult);
+        const searchResult = await searchPlantName(plantDTO);
+        res.status(200).json(searchResult);
     } catch (error) {
         next(error);
     }
 }
 
 const searchByPlantTag = async (req, res, next) => {
+    const plantDTO = req.body.tags.split(',');
     try {
-        const tags = req.body.tags.split(',');
-        const tagSearchResult = await Plant.findAll({
-            attributes: ['id', 'name', 'description', 'thumbnailPath'],
-            include: [{
-                model: Tag,
-                where: {name: {[Op.or]: tags}},
-                attributes: ['id', 'name'],
-                through: {
-                    attributes: []
-                }
-            }]
-        });
-        res.json(tagSearchResult);
+        const tagSearchResult = await searchPlantTag(plantDTO);
+        res.status(200).json(tagSearchResult);
     } catch (error) {
         next(error);
     }

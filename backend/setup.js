2601224ab2a8be593d5bf88aa88faf03bda1be10
Kimsehwan96 <sktpghks138@gmail.com>
2021-02-20 01:07:35 +0900

    [bugfix] remove wrong character

--- backend/setup.js
+++ backend/setup.js
@@ -827,7 +827,7 @@ const reset = async () => {
       imagePath:
         'https://resources.seeat-plant.com/%E1%84%91%E1%85%B5%E1%86%AF%E1%84%85%E1%85%A6%E1%84%8B%E1%85%A1%E1%84%91%E1%85%A6%E1%84%91%E1%85%A6.png',
       description:
-        '- 흙이 마를 때 급수\n' + '물을 좋아하기 때문에 수경재배도 가능',
+        '흙이 마를 때 급수\n' + '물을 좋아하기 때문에 수경재배도 가능',
     },
     {
       PlantId: 10,

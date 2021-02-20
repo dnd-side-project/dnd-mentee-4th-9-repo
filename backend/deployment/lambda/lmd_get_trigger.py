3f3cebdb7fedb4aa396e8260e60b72a0ea0492bd
Kimsehwan96 <sktpghks138@gmail.com>
2021-02-20 22:42:10 +0900

    [feat] add packages that can get cusor from db and logging instance

--- backend/deployment/lambda/lmd_get_trigger.py
+++ backend/deployment/lambda/lmd_get_trigger.py
@@ -2,6 +2,15 @@ import os
 import datetime
 import time
 import boto3
+from db import get_connection
+
+HOST = os.environ.get('DB_HOST', '3.34.87.77')
+PORT = int(os.environ.get('DB_PORT', 3306))
+USER = os.environ.get('DB_USER', 'root')
+TABLE = os.environ.get('DB_TABLE', 'dnd')
+PASSWORD = os.environ.get('DB_PASSWORD', 'j112189')
+
+TEST_QUERY = 'SELECT 1 + 1 AS result'
 
 def handler(event, context):
     print("this lambda has been invoked ")
@@ -12,4 +21,20 @@ def handler(event, context):
         print(f'this is dt object  {dt}')
     else:
         print('No dt got')
+
+    conn = get_connection(HOST, PORT, USER, PASSWORD, TABLE)
+
+    with conn.cursor() as cur:
+        cur.execute(TEST_QUERY)
+        result = cur.fetchall()
+        print(result)
+        return result
+
+if __name__ == "__main__":
+    conn = get_connection(HOST, PORT, USER, PASSWORD, TABLE)
+    with conn.cursor() as cur:
+        cur.execute(TEST_QUERY)
+        result = cur.fetchall()
+        print(result)
+        cur.close()
     

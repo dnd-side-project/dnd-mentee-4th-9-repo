f8ee16612fc8751149cb232886d32bdb9cda3ba1
1000peach <_970813@daum.net>
2021-02-23 23:54:31 +0900

    [feat] change getCuratingResult api request

--- frontend/src/api/plantsAPI.js
+++ frontend/src/api/plantsAPI.js
@@ -5,8 +5,7 @@ export const getPlantDetail = (plantId) => {
 };
 
 export const getCuratingResult = (result) => {
-  // return get('/plants/curating', {params: {result}}); // origin
-  return get('/plants/curating', {params: {result: '몬스테라'}}); // test
+  return get(`/plants/curating?result=${result}`);
 };
 
 export const getAllTags = () => {

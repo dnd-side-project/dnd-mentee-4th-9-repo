431b79f9a20d221fbdf3c24210154fb26df1e818
choisohyun <sohyun4283@naver.com>
2021-02-24 21:48:00 +0900

    [fix] nav hamburger icon style

--- frontend/src/components/Nav/Nav.jsx
+++ frontend/src/components/Nav/Nav.jsx
@@ -60,6 +60,7 @@ function Nav() {
       </Section>
       {matches && (
         <MenuWrapper isOpen={isOpen}>
+          <img onClick={toggleMenuList} src={`${process.env.PUBLIC_URL}/images/hamburger_green.svg`} alt="open hamburger icon" />
           {menuList.map(({name, path}) => (
             <MenuList isOpen={isOpen} key={name} onClick={closeMenuList}>
               <Menu to={path} onClick={(evt) => disableMenu(evt, path)} $isCurrentMenu={path === location.pathname}>
@@ -127,6 +128,11 @@ const MenuWrapper = styled.div`
           position: absolute;
           bottom: 23.5px;
           right: 19.5px;
+        }
+        img {
+          position: absolute;
+          top: 16px;
+          right: 20px;
         }
       `;
     }}

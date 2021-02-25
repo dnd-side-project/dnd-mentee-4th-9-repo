6be02ad392216f9add86390ccbd42189c48f770c
Kimsehwan96 <sktpghks138@gmail.com>
2021-02-26 01:37:57 +0900

    [test] react-snap apply

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {render, hydrate} from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
@@ -9,18 +9,35 @@ import GlobalStyle from './styles/GlobalStyle';
 import {ThemeProvider} from 'styled-components';
 import {HelmetProvider} from 'react-helmet-async';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <HelmetProvider>
-        <App />
-      </HelmetProvider>
-      <GlobalStyle />
-    </ThemeProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const rootElement: any = document.getElementById('root');
+
+if (!!rootElement &&rootElement.hasChildNodes()) {
+  hydrate(
+    <React.StrictMode>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <HelmetProvider>
+          <App />
+        </HelmetProvider>
+        <GlobalStyle />
+      </ThemeProvider>
+    </React.StrictMode>,
+    rootElement
+  );
+} else {
+  render(
+    <React.StrictMode>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <HelmetProvider>
+          <App />
+        </HelmetProvider>
+        <GlobalStyle />
+      </ThemeProvider>
+    </React.StrictMode>,
+    rootElement
+  );
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))

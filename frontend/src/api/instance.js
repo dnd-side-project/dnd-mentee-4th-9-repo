c43196feda5cc35e0ceed053f5b7188da71d9cd0
choisohyun <sohyun4283@naver.com>
2021-02-25 14:33:47 +0900

    [feat] add header api key

--- frontend/src/api/instance.js
+++ frontend/src/api/instance.js
@@ -3,6 +3,9 @@ import {BASE_URL} from '../const/config';
 
 const instance = axios.create({
   baseURL: BASE_URL,
+  headers: {
+    'api-key': '28308fa3aca32470631c27377bbc3f43:a17bf428d90e22d3232e8e95bc4c5b1f',
+  },
 });
 
 const get = (path, request = null) => {

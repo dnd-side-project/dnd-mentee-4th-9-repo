283e299a8294d73088634b3ffe284d88196edbca
Kimsehwan96 <sktpghks138@gmail.com>
2021-03-17 01:52:30 +0900

    start to migration api server

---
+++ backend/msa_migration/plant/test.ts
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports.handler = (event, context, callback) => {
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify({"result" : "success"}),
+    };
+    callback(null, response);
+};

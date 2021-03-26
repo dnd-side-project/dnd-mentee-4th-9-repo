ff2c0b1d15c646ecdf5b3fddb9af78df1e978d54
Kimsehwan96 <sktpghks138@gmail.com>
2021-03-27 00:11:26 +0900

    remove unused backend codes

--- backend/msa_migration/plant/test.ts
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports.handler = (event, context, callback) => {
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify({"result" : "success"}),
-    };
-    callback(null, response);
-};
